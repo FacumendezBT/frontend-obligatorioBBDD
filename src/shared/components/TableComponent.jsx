a6527167ff1b333162c0a5ac65cbde0aaea75b9f
Eze <eze.gonzalez@vera.com.uy>
2024-11-25 23:37:54 -0300

    call me mario cause i'm making some spaghetti

--- src/shared/components/TableComponent.jsx
+++ src/shared/components/TableComponent.jsx
@@ -9,42 +9,48 @@ import { Paper, Box } from '@mui/material';
  * @param {Array} data - Arreglo de objetos que representan las filas.
  * @param {Function} [getRowId] - Función para que vos le pases cual es la key de cada fila.
  */
-const TableComponent = ({ columns, data, getRowId }) => {
-  const dataGridColumns = columns.map((col) => ({
-    field: col.key,
-    headerName: col.nombre,
-    flex: 1,
-    minWidth: 100,
-  }));
+const TableComponent = ({ columns, data, getRowId, setSelected }) => {
+    const dataGridColumns = columns.map((col) => ({
+        field: col.key,
+        headerName: col.nombre,
+        flex: 1,
+        minWidth: 100,
+    }));
 
-  // En caso de que no se pase la función getRowId, usamos esta por defecto, medio fea pero funciona.
-  const defaultGetRowId = (row, index) => row.id || index;
+    // En caso de que no se pase la función getRowId, usamos esta por defecto, medio fea pero funciona.
+    const defaultGetRowId = (row, index) => row.id || index;
 
-  return (
-    <Paper elevation={2} sx={{ width: '100%', height: '100%', padding: 0}}>
-      <Box sx={{ height: '100%', width: '100%' }}>
-        <DataGrid
-          rows={data}
-          columns={dataGridColumns}
-          getRowId={getRowId || defaultGetRowId}
-          pageSize={5}
-          rowsPerPageOptions={[5, 10]}
-          checkboxSelection
-          disableSelectionOnClick
-          sx={{
-            border: 0,
-            '& .MuiDataGrid-cell': {
-              whiteSpace: 'normal',
-              wordWrap: 'break-word',
-            },
-            '& .MuiDataGrid-root': {
-              height: '100%',
-            },
-          }}
-        />
-      </Box>
-    </Paper>
-  );
+    return (
+        <Paper elevation={2} sx={{ width: '100%', height: '100%', padding: 0 }}>
+            <Box sx={{ height: '100%', width: '100%' }}>
+                <DataGrid
+                    rows={data}
+                    columns={dataGridColumns}
+                    getRowId={getRowId || defaultGetRowId}
+                    pageSize={5}
+                    rowsPerPageOptions={[5, 10]}
+                    checkboxSelection
+                    disableSelectionOnClick
+                    onRowSelectionModelChange={
+                        (selection) => {
+                            setSelected(selection)
+                        }
+                    }
+                    //rowSelectionModel={selected}
+                    sx={{
+                        border: 0,
+                        '& .MuiDataGrid-cell': {
+                            whiteSpace: 'normal',
+                            wordWrap: 'break-word',
+                        },
+                        '& .MuiDataGrid-root': {
+                            height: '100%',
+                        },
+                    }}
+                />
+            </Box>
+        </Paper>
+    );
 };
 
 export default TableComponent;
